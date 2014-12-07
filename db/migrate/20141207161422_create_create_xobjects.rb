b7f082483b09a3257ca8687acdb8e600ce73f88e
IljaGoushcha <iljagou@yahoo.com>
2014-12-07 11:18:24 -0500

    fixed migration

--- db/migrate/20141207161422_create_create_xobjects.rb
+++ db/migrate/20141207161422_create_create_xobjects.rb
@@ -1,6 +1,6 @@
-class CreateCreateXobjects < ActiveRecord::Migration
+class CreateXobjects < ActiveRecord::Migration
   def change
-    create_table :create_xobjects do |t|
+    create_table :xobjects do |t|
       t.string :uid, index: true, null: false, unique: true
       t.string :arb_object
 
